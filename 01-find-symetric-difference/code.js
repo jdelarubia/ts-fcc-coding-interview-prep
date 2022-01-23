9fc165a6f9f3baf76c5183efac4cd030b36f39bc
jdelarubia <jdelarubia@gmail.com>
2022-01-23 12:39:24 +0100

    find the symetric difference

---
+++ 01-find-symetric-difference/code.js
@@ -0,0 +1,49 @@
+"use strict";
+// ----------------------------------------------
+// Original assignment
+// ----------------------------------------------
+function sym(...args) {
+    let one = new Set();
+    [...arguments].forEach((subArray) => {
+        const subSet = Array.from(new Set(subArray));
+        subSet.forEach((elem) => {
+            if (!one.has(elem))
+                one.add(elem);
+            else
+                one.delete(elem);
+        });
+    });
+    return Array.from(one).sort();
+} //.
+// sym([1, 2, 3], [5, 2, 1, 4]);
+// ----------------------------------------------
+// Test suite
+// ----------------------------------------------
+const symTests = [
+    { actual: sym([1, 2, 3, 3], [5, 2, 1, 4]), expected: [3, 4, 5], len: 3 },
+    { actual: sym([1, 2, 3, 3], [5, 2, 1, 4]), expected: [3, 4, 5], len: 3 },
+    { actual: sym([1, 2, 3], [5, 2, 1, 4, 5]), expected: [3, 4, 5], len: 3 },
+    { actual: sym([1, 2, 5], [2, 3, 5], [3, 4, 5]), expected: [1, 4, 5], len: 3 },
+    {
+        actual: sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]),
+        expected: [1, 4, 5],
+        len: 3,
+    },
+    {
+        actual: sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]),
+        expected: [2, 3, 4, 6, 7],
+        len: 5,
+    },
+    {
+        actual: sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]),
+        expected: [1, 2, 4, 5, 6, 7, 8, 9],
+        len: 8,
+    },
+];
+symTests.forEach((testResult) => {
+    const { actual, expected, len } = testResult;
+    console.log(`comparing obtained: ${actual} with expected: ${expected}`);
+    console.assert(JSON.stringify(actual) === JSON.stringify(expected), `KO! obtained: ${actual} Vs expected: ${expected}`);
+    console.assert(actual.length === len, `KO! obtained: ${len} Vs expected: ${expected.length}`);
+});
+//# sourceMappingURL=code.js.map
